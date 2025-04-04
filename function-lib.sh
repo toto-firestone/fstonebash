5e9642932fb24c6cdb68146ffc96683b54fb4824
toto-firestone <toto@firestone.kwak>
2025-04-04 19:47:51 +0200

    repackage action functions for auto run scripts + anti AD

--- function-lib.sh
+++ function-lib.sh
@@ -29,6 +29,10 @@ set_mouse_coordinates() {
 	eval $cmd_Y
 }
 
+### ### ### ### ### ###
+## MOTION FUNCTIONS  ##
+### ### ### ### ### ###
+
 click_and_go() {
 	if [ -n "$3" ]; then
 		echo "let's move to $3"
@@ -84,3 +88,68 @@ anti_ad() {
 	click_and_go $X_game_tab $Y_game_tab "anti-ad"
 }
 
+### ### ### ### ### ###
+## ACTION FUNCTIONS  ##
+### ### ### ### ### ###
+
+launch_and_claim_expedition() {
+	go_to_town
+	sleep 1
+	xdotool mousemove $X_guild_portal $Y_guild_portal click 1
+	sleep 1
+	xdotool mousemove $X_exped $Y_exped click 1
+	sleep 2
+	xdotool mousemove $X_exped_but $Y_exped_but click 1
+	sleep 4
+	xdotool mousemove $X_exped_but $Y_exped_but click 1
+}
+
+launch_and_claim_rituals() {
+	go_to_oracle
+	sleep 1
+	xdotool mousemove $X_ritual $Y_ritual click 1
+	sleep 2
+	xdotool mousemove $X_ritual_1 $Y_ritual_1 click 1
+	sleep 4
+	xdotool mousemove $X_ritual_1 $Y_ritual_1 click 1
+	sleep 4
+	xdotool mousemove $X_ritual_2 $Y_ritual_2 click 1
+	sleep 4
+	xdotool mousemove $X_ritual_2 $Y_ritual_2 click 1
+	sleep 4
+	xdotool mousemove $X_ritual_3 $Y_ritual_3 click 1
+	sleep 4
+	xdotool mousemove $X_ritual_3 $Y_ritual_3 click 1
+	sleep 4
+	xdotool mousemove $X_ritual_4 $Y_ritual_4 click 1
+	sleep 4
+	xdotool mousemove $X_ritual_4 $Y_ritual_4 click 1
+}
+
+train_guardian() {
+	go_to_guardian
+	sleep 2
+	xdotool mousemove $X_guard $Y_guard click 1
+	sleep 2
+	xdotool mousemove $X_guard_train $Y_guard_train click 1
+}
+
+claim_campaign_loot() {
+	go_to_map
+	sleep 1
+	xdotool mousemove $X_campaign $Y_campaign click 1
+	sleep 2
+	xdotool mousemove $X_campaign_loot $Y_campaign_loot click 1
+}
+
+claim_tools() {
+	go_to_town
+	sleep 1
+	xdotool mousemove $X_engi $Y_engi click 1
+	sleep 1
+	xdotool mousemove $X_engi_shop $Y_engi_shop click 1
+	sleep 2
+	xdotool mousemove $X_toolclaim $Y_toolclaim click 1
+}
+
+### ### ### ### ###
--- multi-cycle-run.sh
+++ multi-cycle-run.sh
@@ -38,83 +38,6 @@ else
 fi
 echo "very basic checks performed..."
 
-### ### ### ###
-# retype the functions and make some changes
-
-focus_and_go_to_town() {
-	focus_and_back_to_root_screen
-	sleep 1
-	xdotool key t
-}
-
-# not same name as in auto-cycle-config.sh
-launch_and_claim_expedition() {
-	focus_and_go_to_town
-	sleep 1
-	xdotool mousemove $X_guild_portal $Y_guild_portal click 1
-	sleep 1
-	xdotool mousemove $X_exped $Y_exped click 1
-	sleep 2
-	xdotool mousemove $X_exped_but $Y_exped_but click 1
-	sleep 4
-	xdotool mousemove $X_exped_but $Y_exped_but click 1
-}
-
-launch_and_claim_rituals() {
-	focus_and_back_to_root_screen
-	sleep 1
-	xdotool key o
-	sleep 1
-	xdotool mousemove $X_ritual $Y_ritual click 1
-	sleep 2
-	xdotool mousemove $X_ritual_1 $Y_ritual_1 click 1
-	sleep 4
-	xdotool mousemove $X_ritual_1 $Y_ritual_1 click 1
-	sleep 4
-	xdotool mousemove $X_ritual_2 $Y_ritual_2 click 1
-	sleep 4
-	xdotool mousemove $X_ritual_2 $Y_ritual_2 click 1
-	sleep 4
-	xdotool mousemove $X_ritual_3 $Y_ritual_3 click 1
-	sleep 4
-	xdotool mousemove $X_ritual_3 $Y_ritual_3 click 1
-	sleep 4
-	xdotool mousemove $X_ritual_4 $Y_ritual_4 click 1
-	sleep 4
-	xdotool mousemove $X_ritual_4 $Y_ritual_4 click 1
-}
-
-train_guardian() {
-	focus_and_back_to_root_screen
-	sleep 1
-	xdotool key g
-	sleep 1
-	xdotool mousemove $X_guard $Y_guard click 1
-	sleep 2
-	xdotool mousemove $X_guard_train $Y_guard_train click 1
-}
-
-claim_campaign_loot() {
-	focus_and_back_to_root_screen
-	sleep 1
-	xdotool key m
-	sleep 1
-	xdotool mousemove $X_campaign $Y_campaign click 1
-	sleep 2
-	xdotool mousemove $X_campaign_loot $Y_campaign_loot click 1
-}
-
-claim_tools() {
-	focus_and_go_to_town
-	sleep 1
-	xdotool mousemove $X_engi $Y_engi click 1
-	sleep 1
-	xdotool mousemove $X_engi_shop $Y_engi_shop click 1
-	sleep 2
-	xdotool mousemove $X_toolclaim $Y_toolclaim click 1
-}
-
-### ### ### ###
 
 # disable with empty string
 testing=""
@@ -125,6 +48,7 @@ while true; do
 	echo "meta cycle ${i}"
 
 	for i_serv in $@; do
+		anti_ad
 		./switch-server.sh $i_serv
 		source switch.conf
 
@@ -146,6 +70,7 @@ while true; do
 			echo "skip manual mode for testing"
 		fi
 
+		anti_ad
 		launch_and_claim_expedition
 		launch_and_claim_rituals
 		train_guardian
