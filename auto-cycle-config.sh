f5d80d19b67c87f5ed24ca1794b9f72bb466a0c3
toto-firestone <toto@firestone.kwak>
2025-04-04 19:02:08 +0200

    usage of new set_mouse_coordinates

--- auto-cycle-config.sh
+++ auto-cycle-config.sh
@@ -56,11 +56,6 @@ go_to_town() {
 	xdotool key t
 }
 
-prompt_mouse_position() {
-	echo "click back to the terminal where the script is running"
-	read -p "put your mouse pointer above $1 and hit return key"
-}
-
 ### ### ### ###
 
 # Show the way to expedition launch zone
@@ -74,26 +69,17 @@ echo "let the script go to town first"
 xdotool windowactivate $gamewin_id
 #back_to_root_screen
 go_to_town
-prompt_mouse_position "guild portal"
-mouseloc=$(xdotool getmouselocation)
-X_guild_portal=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_guild_portal=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "guild portal" "X_guild_portal" "Y_guild_portal"
 echo "guild portal at x: $X_guild_portal, y: $Y_guild_portal"
 click_and_go $X_guild_portal $Y_guild_portal "guild portal"
 echo
 echo "now let's find the expedition building"
-prompt_mouse_position "expedition building"
-mouseloc=$(xdotool getmouselocation)
-X_exped=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_exped=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "expedition building" "X_exped" "Y_exped"
 echo "expedition building at x: $X_exped, y: $Y_exped"
 click_and_go $X_exped $Y_exped "expedition building"
 echo
 echo "now let's find the expedition launch and claim button"
-prompt_mouse_position "expedition launch and claim button"
-mouseloc=$(xdotool getmouselocation)
-X_exped_but=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_exped_but=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "expedition launch and claim" "X_exped_but" "Y_exped_but"
 echo "expedition button at x: $X_exped_but, y: $Y_exped_but"
 echo
 
@@ -132,33 +118,18 @@ sleep 1
 xdotool key o
 echo
 echo "now let's find the rituals room"
-prompt_mouse_position "ritual icon"
-mouseloc=$(xdotool getmouselocation)
-X_ritual=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_ritual=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "ritual icon" "X_ritual" "Y_ritual"
 echo "ritual icon at x: $X_ritual, y: $Y_ritual"
 click_and_go $X_ritual $Y_ritual "ritual room"
 echo
 echo "now let's get the 4 rituals buttons"
-prompt_mouse_position "ritual 1"
-mouseloc=$(xdotool getmouselocation)
-X_ritual_1=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_ritual_1=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "ritual 1" "X_ritual_1" "Y_ritual_1"
 echo "ritual button 1 at x: $X_ritual_1, y: $Y_ritual_1"
-prompt_mouse_position "ritual 2"
-mouseloc=$(xdotool getmouselocation)
-X_ritual_2=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_ritual_2=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "ritual 2" "X_ritual_2" "Y_ritual_2"
 echo "ritual button 2 at x: $X_ritual_2, y: $Y_ritual_2"
-prompt_mouse_position "ritual 3"
-mouseloc=$(xdotool getmouselocation)
-X_ritual_3=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_ritual_3=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "ritual 3" "X_ritual_3" "Y_ritual_3"
 echo "ritual button 3 at x: $X_ritual_3, y: $Y_ritual_3"
-prompt_mouse_position "ritual 4"
-mouseloc=$(xdotool getmouselocation)
-X_ritual_4=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_ritual_4=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "ritual 4" "X_ritual_4" "Y_ritual_4"
 echo "ritual button 4 at x: $X_ritual_4, y: $Y_ritual_4"
 echo
 
@@ -209,17 +180,11 @@ sleep 1
 xdotool key g
 echo
 echo "select the guardian you want to train"
-prompt_mouse_position "guardian"
-mouseloc=$(xdotool getmouselocation)
-X_guard=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_guard=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "guardian" "X_guard" "Y_guard"
 echo "guardian to train at x: $X_guard, y: $Y_guard"
 echo
 echo "locate the guardian training button"
-prompt_mouse_position "training button"
-mouseloc=$(xdotool getmouselocation)
-X_guard_train=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_guard_train=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "training button" "X_guard_train" "Y_guard_train"
 echo "training button at x: $X_guard_train, y: $Y_guard_train"
 echo
 
@@ -254,18 +219,12 @@ sleep 1
 xdotool key m
 echo
 echo "locate the campaign map selector"
-prompt_mouse_position "campaign map selector"
-mouseloc=$(xdotool getmouselocation)
-X_campaign=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_campaign=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "campaign map selector" "X_campaign" "Y_campaign"
 echo "campaign map select at x: $X_campaign, y: $Y_campaign"
 echo
 click_and_go $X_campaign $Y_campaign "campaign map"
 echo "locate the loot claim button"
-prompt_mouse_position "campaign loot claim"
-mouseloc=$(xdotool getmouselocation)
-X_campaign_loot=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_campaign_loot=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "campaign loot claim" "X_campaign_loot" "Y_campaign_loot"
 echo "campaign loot button at x: $X_campaign_loot, y: $Y_campaign_loot"
 echo
 
@@ -300,26 +259,17 @@ sleep 1
 xdotool key t
 echo
 echo "locate the engineer building"
-prompt_mouse_position "engineer building"
-mouseloc=$(xdotool getmouselocation)
-X_engi=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_engi=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "engineer building" "X_engi" "Y_engi"
 echo "engineer building at x: $X_engi, y: $Y_engi"
 click_and_go $X_engi $Y_engi "engineer building"
 echo
 echo "locate the engineer workshop"
-prompt_mouse_position "engineer workshop"
-mouseloc=$(xdotool getmouselocation)
-X_engi_shop=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_engi_shop=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "engineer workshop" "X_engi_shop" "Y_engi_shop"
 echo "engineer workshop at x: $X_engi_shop, y: $Y_engi_shop"
 click_and_go $X_engi_shop $Y_engi_shop "engineer workshop"
 echo
 echo "locate the tool claim button"
-prompt_mouse_position "tool claim"
-mouseloc=$(xdotool getmouselocation)
-X_toolclaim=$(echo "$mouseloc" | grep -oP 'x:\K\d+')
-Y_toolclaim=$(echo "$mouseloc" | grep -oP 'y:\K\d+')
+set_mouse_coordinates "tool claim" "X_toolclaim" "Y_toolclaim"
 echo "tool claim button at x: $X_toolclaim, y: $Y_toolclaim"
 echo
 
