ffe4c7649efe11a5e978382e1e30b9384c061a53
toto-firestone <toto@firestone.kwak>
2025-04-04 18:51:22 +0200

    new features : terminal id and game tab (anti AD)

--- setwin_id.sh
+++ setwin_id.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source function-lib.sh
 
 # PROMPT USER FOR GAME WINDOW AND SAVE ID IN FILE
 
@@ -9,3 +10,15 @@ gamewin_id=$(xdotool getmouselocation | grep -oP 'window:\K\d+')
 echo id: $gamewin_id
 echo "writing to win_id.conf"
 echo "gamewin_id=${gamewin_id}" > win_id.conf
+
+echo "put your mouse above the terminal window of the scripts"
+read -p "press return key..."
+termwin_id=$(xdotool getmouselocation | grep -oP 'window:\K\d+')
+echo id: $termwin_id
+echo "writing to win_id.conf"
+echo "termwin_id=${termwin_id}" >> win_id.conf
+
+set_mouse_coordinates "game tab selector" "X_game_tab" "Y_game_tab"
+echo "game tab selector at x: $X_game_tab, y: $Y_game_tab"
+echo "X_game_tab=${X_game_tab}" >> win_id.conf
+echo "Y_game_tab=${Y_game_tab}" >> win_id.conf
