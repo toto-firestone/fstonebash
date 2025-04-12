4c50898489f48b3786a2f36831b079312d0c7e42
toto-firestone <toto@firestone.kwak>
2025-04-12 19:25:30 +0200

    Defining usage of this script run it after : ./setwin_id.sh and load the game in browser otherwise, the escrolling feature does not set the view to correct place

--- restore-game-view.sh
+++ restore-game-view.sh
@@ -9,24 +9,33 @@ Y_WIN_POS="2"
 WIN_WIDTH="1430"
 WIN_HEIGHT="947"
 
-echo "MAKE SURE ./setwin_id HAS BEEB RUN BEFORE THIS"
+echo "MAKE SURE ./setwin_id HAS BEEN RUN"
+echo "AND THE GAME IS LOADING OR LOADED BEFORE DOING THIS"
 radish_message "RESTORE GAME STANDARD VIEW"
 
 xdotool windowactivate $gamewin_id
+sleep 1
 echo "selected game window"
 xdotool windowsize --sync $gamewin_id $WIN_WIDTH $WIN_HEIGHT
+sleep 1
 echo "resized"
 xdotool windowmove --sync $gamewin_id $X_WIN_POS $Y_WIN_POS
+sleep 1
 echo "moved to top left"
 
 X_sidebar=$((X_WIN_POS+WIN_WIDTH-30))
 Y_sidebar=$((WIN_HEIGHT/4))
-xdotool mousemove $X_sidebar $Y_sidebar
+xdotool mousemove --sync $X_sidebar $Y_sidebar
+sleep 1
 xdotool click --delay 100 --repeat 15 4
+sleep 1
 
 xdotool mousedown 1
+sleep 1
 echo "hold left button"
 xdotool mousemove_relative --sync 0 112
+sleep 1
 echo "... and drag"
 xdotool mouseup 1
+sleep 1
 echo "release left button"
