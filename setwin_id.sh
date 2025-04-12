74e2b2f03c58f315862ccc44d2cc1aa68e9e0033
toto-firestone <toto@firestone.kwak>
2025-04-12 17:36:06 +0200

    Remove 1 prompt for terminal window ID

--- setwin_id.sh
+++ setwin_id.sh
@@ -3,19 +3,14 @@ source function-lib.sh
 
 # PROMPT USER FOR GAME WINDOW AND SAVE ID IN FILE
 
+radish_message_noprompt "RUN THIS AFTER BROWSER AND GAME PLATFORM LOGIN"
+termwin_id=$(xdotool getwindowfocus)
 
 echo "put your mouse above the game window, do not click"
 read -p "press return key..."
 gamewin_id=$(xdotool getmouselocation | grep -oP 'window:\K\d+')
-echo id: $gamewin_id
-echo "writing to win_id.conf"
+echo "gamewin_id: $gamewin_id, termwin_id: $termwin_id"
 echo "gamewin_id=${gamewin_id}" > win_id.conf
-
-echo "put your mouse above the terminal window of the scripts"
-read -p "press return key..."
-termwin_id=$(xdotool getmouselocation | grep -oP 'window:\K\d+')
-echo id: $termwin_id
-echo "writing to win_id.conf"
 echo "termwin_id=${termwin_id}" >> win_id.conf
 
 set_mouse_coordinates "game tab selector" "X_game_tab" "Y_game_tab"
