b77147718db3d7da9f0a8001de6385931f72ab4e
toto-firestone <toto@firestone.kwak>
2025-04-20 20:40:40 +0200

    Adding Map mission learning tool

---
+++ learning.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+source function-lib.sh
+source win_id.conf
+source view.conf
+
+radish_message_noprompt "Learning tools for map automation"
+source map.conf
+
+if [ ! -d "./map-lab/" ]; then
+	echo "Error : ./map-lab/ directory not found"
+	exit 1
+fi
+
+if [ ! -d "./map-lab/raw-data/" ]; then
+	echo "Error : ./map-lab/raw-data/ directory not found"
+	exit 1
+fi
+
+DATA_FILE="./map-lab/raw-data/$CURR_DATAFILE"
+
+if [ -f "$DATA_FILE" ]; then
+	echo "data file found : $DATA_FILE"
+else
+	echo "creating datafile : $DATA_FILE"
+	touch $DATA_FILE
+fi
+
+#
+# Defining constatants
+#
+
+TAG_EXP="1"
+TAG_ADV="2"
+TAG_WAR="3"
+TAG_MON="4"
+TAG_DRA="5"
+TAG_NAV="6"
+
+MISSION_LIST="exploration adventure war monster dragon naval"
+
+
+while true; do
+	# restart with fresh view at each input
+	# it's quite complex to figure out possible game states
+	# after failure, cancelation
+	go_to_map
+	set_mouse_coordinates "a mission to learn" "X_mission" "Y_mission"
+	test_and_exit $X_mission $Y_mission
+	echo "set the type of this map mission"
+	select i_mission in $MISSION_LIST; do
+		break;
+	done
+	if [ -n "$i_mission" ]; then
+		echo "selected ${i_mission}"
+	else
+		echo "nothing valid selected"
+	fi
+	case $i_mission in
+		exploration ) i_tag=$TAG_EXP;;
+		adventure ) i_tag=$TAG_ADV;;
+		war ) i_tag=$TAG_WAR;;
+		monster ) i_tag=$TAG_MON;;
+		dragon ) i_tag=$TAG_DRA;;
+		naval ) i_tag=$TAG_NAV;;
+		* ) echo "Unknown mission : exit now"
+			break;;
+	esac
+	echo "tag=$i_tag"
+
+	click_and_go $X_mission $Y_mission "testing coordinates"
+	xdotool windowactivate --sync $termwin_id
+	echo "type any NON SPACE KEY + RETURN to validate"
+	read -p "otherwise NOTHING + RETURN to cancel > " i_valid
+	if [ -n "$i_valid" ]; then
+		csv_line="$X_mission,$Y_mission,$i_tag"
+	else
+		echo
+		echo "invalid coordinates : cancel and continue"
+		echo
+		continue;
+	fi
+	# NOT REACHED IF NO csv_line
+	echo
+	echo "going to write : $csv_line"
+	echo
+	echo $csv_line >> $DATA_FILE
+done
