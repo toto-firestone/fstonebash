102ee5a72bf1d18f100c76e7d998a0dbba2dddcb
toto-firestone <toto@firestone.kwak>
2025-05-04 00:46:34 +0200

    extra click on closing gem spending window in case an experiment fails to start despite 1s click hold the non started experiment will be started during claim then the restart after claim will open a gem spend confirmation window closing it should allow to move on to next experiment to start for real and hopefully fix the timing issue

--- auto-alch.sh
+++ auto-alch.sh
@@ -18,7 +18,9 @@ alchemy_special_click() {
 	xdotool mousedown 1
 	sleep 1
 	xdotool mouseup 1
-	sleep .5
+	sleep 1
+	# extra click in case timing is screwed up
+	move_wait_click $X_alch_close_gem $Y_alch_close_gem 2
 }
 
 claim_alch_experiments() {
