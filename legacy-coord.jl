8a652e7d2ea109771711d91090e71d0d4a6114aa
toto-firestone <toto@firestone.kwak>
2025-07-19 20:02:59 +0200

    editable coordinate database. initial state

---
+++ legacy-coord.jl
@@ -0,0 +1,180 @@
+COORD_DB = [
+fstone_coordinate("serv_i", "s1", "X_serv_i", "Y_serv_i", "898", "616", "category", "description")
+;
+fstone_coordinate("server_switch", "s1", "X_server_switch", "Y_server_switch", "1046", "593", "category", "description")
+;
+fstone_coordinate("fav_servers", "s1", "X_fav_servers", "Y_fav_servers", "906", "285", "category", "description")
+;
+fstone_coordinate("serv_confirm", "s1", "X_serv_confirm", "Y_serv_confirm", "784", "652", "category", "description")
+;
+fstone_coordinate("guild_portal", "s1.firestone.conf", "X_guild_portal", "Y_guild_portal", "1048", "285", "category", "description")
+;
+fstone_coordinate("exped", "s1.firestone.conf", "X_exped", "Y_exped", "226", "409", "category", "description")
+;
+fstone_coordinate("exped_but", "s1.firestone.conf", "X_exped_but", "Y_exped_but", "944", "389", "category", "description")
+;
+fstone_coordinate("ritual", "s1.firestone.conf", "X_ritual", "Y_ritual", "593", "457", "category", "description")
+;
+fstone_coordinate("ritual_1", "s1.firestone.conf", "X_ritual_1", "Y_ritual_1", "835", "750", "category", "description")
+;
+fstone_coordinate("ritual_2", "s1.firestone.conf", "X_ritual_2", "Y_ritual_2", "1133", "510", "category", "description")
+;
+fstone_coordinate("ritual_3", "s1.firestone.conf", "X_ritual_3", "Y_ritual_3", "838", "513", "category", "description")
+;
+fstone_coordinate("ritual_4", "s1.firestone.conf", "X_ritual_4", "Y_ritual_4", "1124", "758", "category", "description")
+;
+fstone_coordinate("guard", "s1.firestone.conf", "X_guard", "Y_guard", "741", "847", "category", "description")
+;
+fstone_coordinate("guard_train", "s1.firestone.conf", "X_guard_train", "Y_guard_train", "812", "708", "category", "description")
+;
+fstone_coordinate("campaign", "s1.firestone.conf", "X_campaign", "Y_campaign", "1271", "581", "category", "description")
+;
+fstone_coordinate("campaign_loot", "s1.firestone.conf", "X_campaign_loot", "Y_campaign_loot", "150", "841", "category", "description")
+;
+fstone_coordinate("engi", "s1.firestone.conf", "X_engi", "Y_engi", "906", "745", "category", "description")
+;
+fstone_coordinate("engi_shop", "s1.firestone.conf", "X_engi_shop", "Y_engi_shop", "454", "561", "category", "description")
+;
+fstone_coordinate("toolclaim", "s1.firestone.conf", "X_toolclaim", "Y_toolclaim", "1125", "656", "category", "description")
+;
+fstone_coordinate("alch_close_gem", "alch.conf", "X_alch_close_gem", "Y_alch_close_gem", "0", "0", "category", "description")
+;
+fstone_coordinate("guild_hall", "auto-accept.conf", "X_guild_hall", "Y_guild_hall", "776", "485", "category", "description")
+;
+fstone_coordinate("applications", "auto-accept.conf", "X_applications", "Y_applications", "657", "850", "category", "description")
+;
+fstone_coordinate("accept_player", "auto-accept.conf", "X_accept_player", "Y_accept_player", "854", "317", "category", "description")
+;
+fstone_coordinate("oracle_shop", "collect.conf", "X_oracle_shop", "Y_oracle_shop", "587", "703", "category", "description")
+;
+fstone_coordinate("oracle_gift", "collect.conf", "X_oracle_gift", "Y_oracle_gift", "476", "569", "category", "description")
+;
+fstone_coordinate("main_shop", "collect.conf", "X_main_shop", "Y_main_shop", "922", "432", "category", "description")
+;
+fstone_coordinate("free_box", "collect.conf", "X_free_box", "Y_free_box", "469", "631", "category", "description")
+;
+fstone_coordinate("checkin_tab", "collect.conf", "X_checkin_tab", "Y_checkin_tab", "989", "239", "category", "description")
+;
+fstone_coordinate("checkin_but", "collect.conf", "X_checkin_but", "Y_checkin_but", "960", "744", "category", "description")
+;
+fstone_coordinate("exo_toggle", "collect.conf", "X_exo_toggle", "Y_exo_toggle", "1246", "330", "category", "description")
+;
+fstone_coordinate("speed_scroll", "collect.conf", "X_speed_scroll", "Y_speed_scroll", "673", "580", "category", "description")
+;
+fstone_coordinate("damage_scroll", "collect.conf", "X_damage_scroll", "Y_damage_scroll", "909", "581", "category", "description")
+;
+fstone_coordinate("health_scroll", "collect.conf", "X_health_scroll", "Y_health_scroll", "1144", "580", "category", "description")
+;
+fstone_coordinate("midas_touch", "collect.conf", "X_midas_touch", "Y_midas_touch", "695", "808", "category", "description")
+;
+fstone_coordinate("drum_war", "collect.conf", "X_drum_war", "Y_drum_war", "1153", "374", "category", "description")
+;
+fstone_coordinate("dragon_armor", "collect.conf", "X_dragon_armor", "Y_dragon_armor", "686", "588", "category", "description")
+;
+fstone_coordinate("guardian_rune", "collect.conf", "X_guardian_rune", "Y_guardian_rune", "923", "589", "category", "description")
+;
+fstone_coordinate("totem_agony", "collect.conf", "X_totem_agony", "Y_totem_agony", "1136", "583", "category", "description")
+;
+fstone_coordinate("totem_annihil", "collect.conf", "X_totem_annihil", "Y_totem_annihil", "678", "812", "category", "description")
+;
+fstone_coordinate("ftree_tab", "ftree.conf", "X_ftree_tab", "Y_ftree_tab", "1256", "594", "category", "description")
+;
+fstone_coordinate("ftree_start", "ftree.conf", "X_ftree_start", "Y_ftree_start", "571", "691", "category", "description")
+;
+fstone_coordinate("ftree_claim", "ftree.conf", "X_ftree_claim", "Y_ftree_claim", "429", "828", "category", "description")
+;
+fstone_coordinate("map_mission_start", "map.conf", "X_map_mission_start", "Y_map_mission_start", "696", "792", "category", "description")
+;
+fstone_coordinate("map_mission_claim", "map.conf", "X_map_mission_claim", "Y_map_mission_claim", "156", "385", "category", "description")
+;
+fstone_coordinate("WIN_POS", "view.conf", "X_WIN_POS", "Y_WIN_POS", "0", "0", "category", "description")
+;
+fstone_coordinate("camp_map", "wm.conf", "X_camp_map", "Y_camp_map", "1271", "582", "category", "description")
+;
+fstone_coordinate("mission_button", "wm.conf", "X_mission_button", "Y_mission_button", "1228", "857", "category", "description")
+;
+fstone_coordinate("liberations", "wm.conf", "X_liberations", "Y_liberations", "501", "724", "category", "description")
+;
+fstone_coordinate("dungeons", "wm.conf", "X_dungeons", "Y_dungeons", "874", "720", "category", "description")
+;
+fstone_coordinate("dungeon_1", "wm.conf", "X_dungeon_1", "Y_dungeon_1", "544", "697", "category", "description")
+;
+fstone_coordinate("dungeon_2", "wm.conf", "X_dungeon_2", "Y_dungeon_2", "827", "699", "category", "description")
+;
+fstone_coordinate("liberation_1", "wm.conf", "X_liberation_1", "Y_liberation_1", "234", "697", "category", "description")
+;
+fstone_coordinate("liberation_2", "wm.conf", "X_liberation_2", "Y_liberation_2", "516", "697", "category", "description")
+;
+fstone_coordinate("liberation_3", "wm.conf", "X_liberation_3", "Y_liberation_3", "794", "695", "category", "description")
+;
+fstone_coordinate("liberation_4", "wm.conf", "X_liberation_4", "Y_liberation_4", "1047", "695", "category", "description")
+;
+fstone_coordinate("north_island", "brute-force.sh", "X_north_island", "Y_north_island", "315", "811", "category", "description")
+;
+fstone_coordinate("battle_building", "brute-force.sh", "X_battle_building", "Y_battle_building", "250", "296", "category", "description")
+;
+fstone_coordinate("campaign_access", "brute-force.sh", "X_campaign_access", "Y_campaign_access", "562", "529", "category", "description")
+;
+fstone_coordinate("guild_shop", "claim-pickaxe.sh", "X_guild_shop", "Y_guild_shop", "464", "337", "category", "description")
+;
+fstone_coordinate("guild_supplies", "claim-pickaxe.sh", "X_guild_supplies", "Y_guild_supplies", "159", "689", "category", "description")
+;
+fstone_coordinate("claim_pickaxe", "claim-pickaxe.sh", "X_claim_pickaxe", "Y_claim_pickaxe", "510", "466", "category", "description")
+;
+fstone_coordinate("firefox_menu", "firestone-quit.sh", "X_firefox_menu", "Y_firefox_menu", "1357", "93", "category", "description")
+;
+fstone_coordinate("firefox_quit", "firestone-quit.sh", "X_firefox_quit", "Y_firefox_quit", "1298", "772", "category", "description")
+;
+fstone_coordinate("load", "firestone-starter.sh", "X_load", "Y_load", "678", "565", "category", "description")
+;
+fstone_coordinate("avatar", "firestone-starter.sh", "X_avatar", "Y_avatar", "105", "233", "category", "description")
+;
+fstone_coordinate("more", "friday-code.sh", "X_more", "Y_more", "977", "209", "category", "description")
+;
+fstone_coordinate("code", "friday-code.sh", "X_code", "Y_code", "456", "588", "category", "description")
+;
+fstone_coordinate("submit", "friday-code.sh", "X_submit", "Y_submit", "848", "592", "category", "description")
+;
+fstone_coordinate("daily_quests", "multi-cycle-run.sh", "X_daily_quests", "Y_daily_quests", "552", "258", "category", "description")
+;
+fstone_coordinate("weekly_quests", "multi-cycle-run.sh", "X_weekly_quests", "Y_weekly_quests", "828", "257", "category", "description")
+;
+fstone_coordinate("quests_claim", "multi-cycle-run.sh", "X_quests_claim", "Y_quests_claim", "1116", "368", "category", "description")
+;
+fstone_coordinate("WIN_POS", "restore-game-view.sh", "X_WIN_POS", "Y_WIN_POS", "0", "0", "category", "description")
+;
+fstone_coordinate("sidebar", "restore-game-view.sh", "X_sidebar", "Y_sidebar", "0", "0", "category", "description")
+;
+fstone_coordinate("irongard_ul", "visual-lib.sh", "x_irongard_ul", "y_irongard_ul", "501", "176", "category", "description")
+;
+fstone_coordinate("irongard_br", "visual-lib.sh", "x_irongard_br", "y_irongard_br", "881", "215", "category", "description")
+;
+fstone_coordinate("url_ul", "visual-lib.sh", "x_url_ul", "y_url_ul", "258", "73", "category", "description")
+;
+fstone_coordinate("url_br", "visual-lib.sh", "x_url_br", "y_url_br", "1123", "103", "category", "description")
+;
+fstone_coordinate("startref_ul", "visual-lib.sh", "x_startref_ul", "y_startref_ul", "72", "699", "category", "description")
+;
+fstone_coordinate("startref_br", "visual-lib.sh", "x_startref_br", "y_startref_br", "109", "732", "category", "description")
+;
+fstone_coordinate("servname_ul", "visual-lib.sh", "x_servname_ul", "y_servname_ul", "877", "442", "category", "description")
+;
+fstone_coordinate("servname_br", "visual-lib.sh", "x_servname_br", "y_servname_br", "1210", "555", "category", "description")
+;
+fstone_coordinate("switch_fav_ul", "visual-lib.sh", "x_switch_fav_ul", "y_switch_fav_ul", "352", "211", "category", "description")
+;
+fstone_coordinate("switch_fav_br", "visual-lib.sh", "x_switch_fav_br", "y_switch_fav_br", "1023", "320", "category", "description")
+;
+fstone_coordinate("map_zero_squad_ul", "visual-lib.sh", "x_map_zero_squad_ul", "y_map_zero_squad_ul", "813", "188", "category", "description")
+;
+fstone_coordinate("map_zero_squad_br", "visual-lib.sh", "x_map_zero_squad_br", "y_map_zero_squad_br", "842", "212", "category", "description")
+;
+fstone_coordinate("map_idle_notif_ul", "visual-lib.sh", "x_map_idle_notif_ul", "y_map_idle_notif_ul", "344", "854", "category", "description")
+;
+fstone_coordinate("map_idle_notif_br", "visual-lib.sh", "x_map_idle_notif_br", "y_map_idle_notif_br", "853", "876", "category", "description")
+;
+fstone_coordinate("ftree_rew_test_ul", "visual-lib.sh", "x_ftree_rew_test_ul", "y_ftree_rew_test_ul", "55", "371", "category", "description")
+;
+fstone_coordinate("ftree_rew_test_br", "visual-lib.sh", "x_ftree_rew_test_br", "y_ftree_rew_test_br", "154", "669", "category", "description")
+;
+]
