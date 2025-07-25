933ede923f2710c407860a1fb81249c8568a71a6
toto-firestone <toto@firestone.kwak>
2025-07-25 21:48:19 +0200

    New version of server configuration script. Much simpler

--- server-config.sh
+++ server-config.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source glob-coord.conf
 source function-lib.sh
 
 # THIS IS RADISH AUTOMATION TOOL
@@ -12,59 +11,10 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-echo "Configuring servers : $@"
-source win_id.conf
-echo read id: $gamewin_id
-echo
-
-echo "Setting window"
-go_to_settings
-
-echo "now let's find the server switch button"
-set_mouse_coordinates "server switch" "X_server_switch" "Y_server_switch"
-echo "server switch at x: $X_server_switch, y: $Y_server_switch"
-click_and_go $X_server_switch $Y_server_switch "server switch"
-echo
-
-echo "now let's find the favorite servers button"
-set_mouse_coordinates "favorite servers" "X_fav_servers" "Y_fav_servers"
-echo "favorite servers at x: $X_fav_servers, y: $Y_fav_servers"
-click_and_go $X_fav_servers $Y_fav_servers "favorite servers"
-echo
-
-echo "now configuration of each server's coordiantes"
+echo "Configuring servers config files for : $@"
 for servname in "$@"; do
-	echo "server $servname"
-	set_mouse_coordinates "$servname" "X_serv_i" "Y_serv_i"
-	echo "server $servname x: $X_serv_i, y: $Y_serv_i"
-	echo "Creating/overwriting file $servname"
-	echo "X_serv_i=${X_serv_i}" > $servname
-	echo "Y_serv_i=${Y_serv_i}" >> $servname
+	serv_conf="$servname.firestone.conf"
+	echo "server file : $serv_conf"
+	echo "i_guardian_slot=1" > $serv_conf
 done
 echo
-
-echo "now let's find the servers switch confirmation button"
-echo "Go to game window and select eany server that is not current"
-echo "the confirmation button should appear"
-set_mouse_coordinates "server confirmation" "X_serv_confirm" "Y_serv_confirm"
-echo "server confirmation at x: $X_serv_confirm, y: $Y_serv_confirm"
-echo
-
-echo "completing configuration of each server's coordiantes"
-for servname in "$@"; do
-	echo "server $servname"
-	echo "X_server_switch=${X_server_switch}" >> $servname
-	echo "Y_server_switch=${Y_server_switch}" >> $servname
-	echo "X_fav_servers=${X_fav_servers}" >> $servname
-	echo "Y_fav_servers=${Y_fav_servers}" >> $servname
-	echo "X_serv_confirm=${X_serv_confirm}" >> $servname
-	echo "Y_serv_confirm=${Y_serv_confirm}" >> $servname
-done
-echo 
-
-echo "Initializing current server with switch.conf file"
-read -p "write the current server name and type return > " current_servname
-echo "writing $current_servname to switch file"
-echo "current_servname=${current_servname}" > switch.conf
-echo "configuration done"
-focus_and_back_to_root_screen
