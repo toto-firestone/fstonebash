e2905e92ac92377a39a4d35cc150eb9fcd87bea8
toto-firestone <toto@firestone.kwak>
2025-08-15 19:37:47 +0200

    use new functions in old scripts

--- auto-collect-run.sh
+++ auto-collect-run.sh
@@ -2,60 +2,21 @@
 source glob-coord.conf
 source master.conf
 source function-lib.sh
+source tricky-dailies.sh
 
 radish_message_noprompt "FARMING DAILY CURRENCIES"
 
-xdotool windowactivate --sync $gamewin_id
 anti_ad
-go_to_oracle
-move_wait_click $X_oracle_shop $Y_oracle_shop 2
-move_wait_click $X_oracle_gift $Y_oracle_gift 2
 
-go_to_town
-move_wait_click $X_main_shop $Y_main_shop 2
-move_wait_click $X_free_box $Y_free_box 3
-sleep 1
-move_wait_click $X_checkin_tab $Y_checkin_tab 2
-echo "Warning : check-in button may change place when special offer occurs"
-echo "a wrong check-in coordinate is not harmful"
-move_wait_click $X_checkin_but $Y_checkin_but 2
-echo "try alternative position..."
-move_wait_click $X_checkin_but $((Y_checkin_but+46)) 2
+auto_claim_gifts
+auto_exotic_sales
 
-go_to_exotic
-# toggle to X50
-move_wait_click $X_exo_toggle $Y_exo_toggle 2
-move_wait_click $X_exo_toggle $Y_exo_toggle 3
-sleep 1
-
-move_wait_click $X_speed_scroll $Y_speed_scroll 2
-move_wait_click $X_damage_scroll $Y_damage_scroll 2
-move_wait_click $X_health_scroll $Y_health_scroll 2
-move_wait_click $X_midas_touch $Y_midas_touch 2
-
-echo "scrolling to the bottom part"
-#sleep 2
-smooth_drag_and_drop $X_exotic_scroll $Y_exotic_scroll_bottom $X_exotic_scroll $Y_exotic_scroll_top
-sleep 2
-smooth_drag_and_drop $X_exotic_scroll $Y_exotic_scroll_bottom $X_exotic_scroll $Y_exotic_scroll_top
-
-move_wait_click $X_drum_war $Y_drum_war 2
-move_wait_click $X_dragon_armor $Y_dragon_armor 2
-move_wait_click $X_guardian_rune $Y_guardian_rune 2
-move_wait_click $X_totem_agony $Y_totem_agony 2
-move_wait_click $X_totem_annihil $Y_totem_annihil 2
-
-echo "claim pickaxes"
-./claim-pickaxe.sh
-
-
-source tricky-dailies.sh
+auto_claim_pickaxes
+auto_crystal_5_hit
 
 auto_beer_token_10_pull
+
 auto_scarab_10_pull_and_vault
-auto_crystal_5_hit
+
 auto_open_10_max_chests
 flush_daily_mail
-
-focus_and_back_to_root_screen
-
--- auto-wm-run.sh
+++ auto-wm-run.sh
@@ -2,6 +2,7 @@
 source glob-coord.conf
 source master.conf
 source function-lib.sh
+source tricky-dailies.sh
 
 # THIS IS RADISH AUTOMATION TOOL
 # DAILY WM RUN SCRIPT
@@ -35,95 +36,9 @@ if [[ "$N_liber" -le "0" && "$N_dung" -le "0" ]]; then
 	exit 0
 fi
 
-mission_ok() {
-	xdotool windowactivate --sync $gamewin_id
-	sleep 1
-	xdotool key Escape
-	sleep 5
-}
-
-liberation_click() {
-	move_wait_only $1 $2 $3
-	super_slow_click
-}
-
 ### ### ### ###
-
-xdotool windowactivate --sync $gamewin_id
 anti_ad
-go_to_map
-move_wait_click $X_camp_map $Y_camp_map 4
-move_wait_click $X_mission_button $Y_mission_button 6
 
-move_wait_click $X_liberations $Y_liberations 4
-sleep 4
+auto_liberation_dungeon $N_liber $N_dung $T_fight_sec
 
-i=0
-if [ "$i" -lt  "$N_liber" ]; then
-	i=$((i+1))
-	echo "liberation $i"
-	liberation_click $X_liberation_1 $Y_liberation_1 2
-	sleep $T_fight_sec
-	mission_ok
-fi
-if [ "$i" -lt  "$N_liber" ]; then
-	i=$((i+1))
-	echo "liberation $i"
-	liberation_click $X_liberation_2 $Y_liberation_2 2
-	sleep $T_fight_sec
-	mission_ok
-fi
-if [ "$i" -lt  "$N_liber" ]; then
-	i=$((i+1))
-	echo "liberation $i"
-	liberation_click $X_liberation_3 $Y_liberation_3 2
-	sleep $T_fight_sec
-	mission_ok
-fi
-if [ "$i" -lt  "$N_liber" ]; then
-	i=$((i+1))
-	echo "liberation $i"
-	liberation_click $X_liberation_4 $Y_liberation_4 2
-	sleep $T_fight_sec
-	mission_ok
-fi
-
-# scrolled part
-echo "DON'T MOVE MOUSE NOW"
-xdotool mousemove $X_liberation_4 $Y_liberation_4
-sleep 4
-while [ "$i" -lt  "$N_liber" ]; do
-	i=$((i+1))
-	echo "let's scroll to liberation $i"
-	smooth_drag_and_drop $X_liber_scroll_right $Y_liber_scroll $X_liber_scroll_left $Y_liber_scroll
-
-	#slow_safe_click
-	liberation_click $X_liberation_4 $Y_liberation_4 2
-	sleep $T_fight_sec
-	mission_ok
-	# tempo correction
-	sleep 4
-done
-
-
-mission_ok
-move_wait_click $X_dungeons $Y_dungeons 6
-
-i=0
-if [ "$i" -lt  "$N_dung" ]; then
-	i=$((i+1))
-	echo "dungeons $i"
-	liberation_click $X_dungeon_1 $Y_dungeon_1 2
-	sleep $T_fight_sec
-	mission_ok
-fi
-if [ "$i" -lt  "$N_dung" ]; then
-	i=$((i+1))
-	echo "dungeons $i"
-	liberation_click $X_dungeon_2 $Y_dungeon_2 2
-	sleep $T_fight_sec
-	mission_ok
-fi
-
-focus_and_back_to_root_screen
 echo "Daily WM missions done"
