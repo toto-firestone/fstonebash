284f69b7e1c15cc9d130e9e048bdda29594fc270
toto-firestone <toto@firestone.kwak>
2025-11-24 12:09:36 +0100

    move something that should not be in a config file

--- init-interactive.sh
+++ init-interactive.sh
@@ -13,3 +13,42 @@ source remote-tools.sh
 
 # define now environment variables related to remote or local access
 env_for_remote_or_local
+
+## handling config files templates updates
+update_config_files() {
+	# this array is local
+	declare -a config_files_A=(
+		"auto-accept.conf"
+		"daily.conf"
+		"ftree.conf"
+		"gameover.conf"
+		"glob-coord.conf"
+		"map.conf"
+		"master.conf"
+		"s1.firestone.conf"
+		"s27.firestone.conf"
+		"s31.firestone.conf"
+		"s14.firestone.conf"
+		"s25.firestone.conf"
+		"s8.firestone.conf"
+		"switch.conf"
+		"view.conf"
+		"win_id.conf"
+	)
+	for conf_f in "${config_files_A[@]}"; do
+		echo "** processing $conf_f"
+		local templ_f="templates/$conf_f"
+		diff "$templ_f" "$conf_f"
+
+		local ans
+		read -p "* update this file ? (y/n) " ans
+		if [ "$ans" == "y" ]; then
+			echo "* copy to $templ_f"
+			cp "$conf_f" "$templ_f"
+		else
+			echo "* skip"
+		fi
+		read -p "* press any key to continue "
+		echo
+	done
+}
