62f1b770372e49be831d8aa3f73cee8ebac6b814
toto-firestone <toto@firestone.kwak>
2026-06-23 23:50:09 +0200

    tmp dir backup

---
+++ sample-tmp/s14.treasure_2026_04.jl
@@ -0,0 +1,34 @@
+
+damage = OrderedDict(
+	"Natael80" => 15.7,
+	"Shiftax" => 7.3,
+	"Guest" => 6.3,
+	"Steamy Wombat" => 6.2,
+	"PinkDragon" => 6.2,
+	"Woozy Numbat" => 6.1,
+	"SuperManAdragon" => 5.9,
+	"Snootyx" => 5.6,
+	"Jukkei" => 5.5,
+	"Troudhuk" => 5.4,
+	"Maurice" => 5.2,
+	"toto" => 4.6,
+	"BrianShih" => 4.3,
+	"depoy" => 4.2,
+	"PI PEH" => 2.7,
+	"TrueChiter" => 2.6,
+	"Harry21" => 1.8,
+	"Keimu_1" => 1.0,
+	"Athena" => 1.0,
+	"Behemoth2332" => 0.7,
+	"Arailea" => 0.6,
+	"NRPB" => 0.5,
+	"dRagon" => 0.4,
+	"N." => 0.3
+)
+
+reward = Dict(
+	"tavern" => 21,
+	"dust" => 1019,
+	"contract" => 204,
+	"tome" => 30
+)
